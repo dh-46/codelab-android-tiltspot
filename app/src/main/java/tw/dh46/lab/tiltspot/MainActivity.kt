3c0921b3dca72cae6bcb1034c7c324efad554c7c
danielhuang <danielhuang.work@gmail.com>
2022-11-05 17:10:19 +0800

    Chore: Init project

---
+++ app/src/main/java/tw/dh46/lab/tiltspot/MainActivity.kt
@@ -0,0 +1,11 @@
+package tw.dh46.lab.tiltspot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
